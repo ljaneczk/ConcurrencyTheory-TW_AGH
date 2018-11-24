bb970a0cb07a15bdfe3fb9c63b3392dc8b110c71
Łukasz Janeczko <lukij1997@gmail.com>
2018-11-24 17:34:33 +0100

    Updated main

--- lab4/producer-consumer/src/Main.java
+++ lab4/producer-consumer/src/Main.java
@@ -1,4 +1,6 @@
 import task1.*;
+import task2.*;
+import task2.common.MyRandom;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,9 +13,9 @@ public class Main {
         for (int i = 0; i < n; i++)
             data[i] = new Data(i);
         PipeBuffer pipeBuffer = new PipeBuffer(data);
-        Worker[] workers = new Worker[p]; Thread[] threads = new Thread[p];
+        PipeWorker[] workers = new PipeWorker[p]; Thread[] threads = new Thread[p];
         for (int i = 0; i < p; i++) {
-            workers[i] = new Worker(i, i-1, pipeBuffer);
+            workers[i] = new PipeWorker(i, i-1, pipeBuffer);
             threads[i] = new Thread(workers[i]);
         }
         for (int i = 0; i < p; i++)
@@ -21,6 +23,33 @@ public class Main {
     }
 
     private static void runTask2() {
+        //runTask2Unfair();
+        runTask2Fair();
+    }
 
+    private static void runTask2Unfair() {
+        int n0 = 100, n1 = 100, M = 10000;
+        MyRandom random = new MyRandom(M);
+        UnfairBuffer buffer = new UnfairBuffer(M);
+        Thread[] threads = new Thread[n0+n1];
+        for (int i = 0; i < n0; i++)
+            threads[i] = new Thread(new UnfairProducer(random, buffer, i));
+        for (int i = 0; i < n1; i++)
+            threads[n0+i] = new Thread(new UnfairConsumer(random, buffer, i));
+        for (Thread thread : threads)
+            thread.start();
+    }
+
+    private static void runTask2Fair() {
+        int n0 = 100, n1 = 100, M = 1000;
+        MyRandom random = new MyRandom(M);
+        FairBuffer buffer = new FairBuffer(M);
+        Thread[] threads = new Thread[n0+n1];
+        for (int i = 0; i < n0; i++)
+            threads[i] = new Thread(new FairProducer(random, buffer, i));
+        for (int i = 0; i < n1; i++)
+            threads[n0+i] = new Thread(new FairConsumer(random, buffer, i));
+        for (Thread thread : threads)
+            thread.start();
     }
 }
